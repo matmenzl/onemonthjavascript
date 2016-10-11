d53b79ef35b83f30da3b94c4b042fe61e47c291b
matmenzl <info@mathiasmenzl.ch>
2016-10-11 23:26:22 +0200

    added soundcloud query

--- week3/SoundCloudPlayer/javascript/main.js
+++ week3/SoundCloudPlayer/javascript/main.js
@@ -2,6 +2,17 @@
 
 // 2. Query Soundcloud API
 
+SC.initialize({
+  client_id: '195d273fb18f4a9a75ebda65c1aa2631'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'rilo kiley'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
 // 3. Populate/Display Cards
 
 // 4. Populate Playlist and Play
