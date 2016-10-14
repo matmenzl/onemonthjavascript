5e3bc7311db7b012ad4c56bec4b27173a6d8f3eb
matmenzl <info@mathiasmenzl.ch>
2016-10-14 10:45:52 +0200

    homework week3 search done

--- week3/SoundCloudPlayer/javascript/main.js
+++ week3/SoundCloudPlayer/javascript/main.js
@@ -1,18 +1,151 @@
 // 1. Search
 
+var UI = {};
+
+UI.enterPress = function() {
+
+  document.querySelector('.js-search').addEventListener('keyup', function(e){
+
+    var inputValue = document.querySelector('input').value;
+    if(e.which===13) 
+    SoundCloudAPI.getTrack(inputValue);
+  });
+
+}
+UI.enterPress();
+
+
+
+UI.submitClick = function() {
+
+  document.querySelector('.js-submit').addEventListener('click', function(){
+
+    var inputValue = document.querySelector('input').value;
+    if (inputValue == '') {
+      alert("Enter a Search Term");
+    } else {
+    SoundCloudAPI.getTrack(inputValue);
+    }
+  });
+}
+
+UI.submitClick();
+
+UI.clearSearch = function() {
+    
+    document.querySelector(".search-results").innerHTML = "";    
+    
+}
+UI.clearSearch();
+
+
+
 // 2. Query Soundcloud API
 
-SC.initialize({
-  client_id: '195d273fb18f4a9a75ebda65c1aa2631'
-});
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'rilo kiley'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+var SoundCloudAPI = {};
+
+SoundCloudAPI.init = function() {
+
+  SC.initialize({
+    client_id: '195d273fb18f4a9a75ebda65c1aa2631'
+  });
+
+}
+
+SoundCloudAPI.init();
+
+SoundCloudAPI.getTrack = function(inputValue) {
+
+  UI.clearSearch();
+
+  SC.get('/tracks', {
+    q: inputValue
+  }).then(function(tracks) {
+    console.log(tracks);
+    SoundCloudAPI.renderTracks(tracks);
+  });
+}
+
+
 
 // 3. Populate/Display Cards
+SoundCloudAPI.renderTracks = function(tracks) {
+
+  tracks.forEach(function(track){
+    //card
+    var card = document.createElement('div');
+    card.classList.add('card');
+
+    var searchResults = document.querySelector(".search-results");
+    searchResults.appendChild(card);
+
+    //image
+    var imageDiv = document.createElement('div');
+    imageDiv.classList.add('image');
+
+    var image_img = document.createElement('img');
+    image_img.classList.add('image_img');
+    image_img.src = track.artwork_url || 'http://lorempixel.com/100/100/abstract/';
+    imageDiv.appendChild(image_img);
+    card.appendChild(imageDiv);
+
+    //content
+    var content = document.createElement('div');
+    content.classList.add('content');
+    card.appendChild(content);
+
+    //header
+    var header = document.createElement('div');
+    header.classList.add('header');
+    header.innerHTML = '<a href="' + track.permalink_url + '" target="_blank">' + track.title + '</a>';
+    content.appendChild(header);
+
+    //button
+    var button = document.createElement('div');
+    button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+    card.appendChild(button);
+
+    //icon
+    var icon = document.createElement('i');
+    icon.classList.add('add', 'icon');
+    button.appendChild(icon);
+
+    //button
+    var buttonText = document.createElement('span');
+    buttonText.innerHTML = 'Add to Playlist';
+    button.appendChild(buttonText);
+
+    //click event
+    button.addEventListener('click', function(){
+      SoundCloudAPI.getEmbed(track.permalink_url);
+    });
+
+
+
+  });
+
+};
+
 
 // 4. Populate Playlist and Play
+
+SoundCloudAPI.getEmbed = function(trackURL) {
+  SC.oEmbed(trackURL, {
+    auto_play: true
+  }).then(function(embed){
+    console.log('oEmbed response: ', embed);
+
+
+
+      var sideBar = document.querySelector('.js-playlist');
+      var box = document.createElement('div');
+      box.innerHTML = embed.html;
+
+      sideBar.insertBefore(box, sideBar.firstChild);
+      localStorage.setItem("key", "sidebar.innerHTML");
+
+  });
+
+};
+
